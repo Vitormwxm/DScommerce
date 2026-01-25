40093bb3cbb8d9c1556facbdcaaf01e78e8f002c
vitor <vitormwxm@gmail.com>
2026-01-25 20:31:50 -0300

    Search product by id

--- src/main/java/com/vitormwxm/dscommece/controllers/ProductController.java
+++ src/main/java/com/vitormwxm/dscommece/controllers/ProductController.java
@@ -1,15 +1,25 @@
 package com.vitormwxm.dscommece.controllers;
 
+import com.vitormwxm.dscommece.entities.Product;
+import com.vitormwxm.dscommece.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController// informando que é uma controller do tipo APIRest
 @RequestMapping( value = "products")
 public class ProductController {
 
+    @Autowired
+    private ProductRepository repository;
+
     @GetMapping // informa que é uma requisição GET
     public String teste() {
-        return "Ola mundo!";
+        Optional<Product> result = repository.findById(1L);
+        Product product = result.get();
+        return product.getName();
     }
 }
