e1445c85ccc12ef49c00b4ab3d29a6401572d524
vitor <vitormwxm@gmail.com>
2026-01-27 20:57:10 -0300

    create method findAll in controller

--- src/main/java/com/vitormwxm/dscommece/controllers/ProductController.java
+++ src/main/java/com/vitormwxm/dscommece/controllers/ProductController.java
@@ -5,12 +5,15 @@ import com.vitormwxm.dscommece.entities.Product;
 import com.vitormwxm.dscommece.repository.ProductRepository;
 import com.vitormwxm.dscommece.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
+import java.util.List;
+
 
 @RestController// informando que é uma controller do tipo APIRest
 @RequestMapping( value = "products")
@@ -22,6 +25,12 @@ public class ProductController {
     @GetMapping(value = "/{id}") // informa que é uma requisição GET, que deve utilizar um id para o produto
     public ProductDTO findById(@PathVariable Long id) { //PathVariable vai casar o caminho completo
         ProductDTO dto = service.findById(id);
+        return dto;
+    }
+
+    @GetMapping// informa que é uma requisição GET, que deve utilizar um id para o produto
+    public Page<ProductDTO> findAll(Pageable pageable) { //PathVariable vai casar o caminho completo
+        Page<ProductDTO> dto = service.findAll(pageable);
         return dto;
     }
 }
